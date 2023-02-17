342d89720737d215814c3793442ff9f80cc5be76
Sicu Bogdan-Andrei <b0gdan.munt3@gmail.com>
2023-02-17 11:28:04 +0200

    added BCrypt.Net package for hashing the passwords

---
+++ JWT tutorial/JWT tutorial/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using JWT_tutorial.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_tutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private static User user = new User();
+
+        public AuthController() { }
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            user.Username = request.UserName; 
+            user.PasswordHash = passwordHash;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserDto request)    
+        {
+            if (user.Username != request.UserName)
+            {
+                return BadRequest("User not found");
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            {
+                return BadRequest("Wrong password");
+            }
+
+            return Ok(user);
+        }
+
+    }
+}
